c070185b49278d115b07b79e0830171a55e866b2
saqib <saqib.aminul29@gmail.com>
2024-10-24 15:59:14 +0600

    update schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { join } from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/schema/schema.gql'),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
