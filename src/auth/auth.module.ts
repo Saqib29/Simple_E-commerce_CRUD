612eaeed7b71339dd37a3a777bcdead25662ee59
saqib <saqib.aminul29@gmail.com>
2024-10-24 22:29:44 +0600

    include jwt module and service and resolver

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { JwtStrategy } from 'src/utils/jwt.strategy';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXP },
+    }),
+  ],
+  providers: [AuthService, AuthResolver, JwtStrategy]
+})
 export class AuthModule {}
