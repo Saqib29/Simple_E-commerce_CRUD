c6ea70820293e0e763d897bec477a6c1cc175d6c
saqib <saqib.aminul29@gmail.com>
2024-10-24 19:21:14 +0600

    impl of userResolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, ID, Query, Resolver } from '@nestjs/graphql';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => User)
+export class UserResolver {
+    constructor(private userService: UserService){}
+
+    @Query(() => [User])
+    async users(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Query(() => User)
+    async user(@Args('id', { type: () => ID }) id: string): Promise<User> {
+        return this.userService.findOne(id);
+    }
+}
