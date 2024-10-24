964eb48b4023249649c3e7c62988b6c6062f6eee
saqib <saqib.aminul29@gmail.com>
2024-10-25 00:43:25 +0600

    impl of auth resolver

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,28 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { AuthPayload } from 'src/types/auth-payload';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+    constructor(private authService: AuthService){}
+
+    @Mutation(() => String)
+    async singin(
+        @Args('email') email: string,
+        @Args('password') password: string,
+    ) {
+        const user = await this.authService.validateUser(email, password);
+        if (!user) {
+            throw new Error('Invalid credentials')
+        }
+        return this.authService.signIn(user);
+    }
+
+    @Mutation(() => AuthPayload)
+    async signup(
+        @Args('email') email: string,
+        @Args('password') password: string,
+    ): Promise<AuthPayload> {
+        return this.authService.signUp(email, password);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { AuthPayload } from 'src/types/auth-payload';
 
 @Injectable()
 export class AuthService {
@@ -19,15 +20,15 @@ export class AuthService {
         return null;
     }
 
-    async signIn(user: any) {
+    async signIn(user: any): Promise<AuthPayload> {
         const payload = { email: user.email, sub: user.id };
         return {
             access_token: this.jwtService.sign(payload)
         };
     }
 
-    async signUp(email: string, password: string) {
-        const hashedPassword = await bcrypt.hash(password, process.env.BCRYPT_SALT);
+    async signUp(email: string, password: string): Promise<AuthPayload> {
+        const hashedPassword = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT));
         const user = await this.userService.create(email, hashedPassword);
 
         return this.signIn(user);
