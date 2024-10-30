346edf6674f148773747cdde28310e967dcba7fb
saqib <saqib.aminul29@gmail.com>
2024-10-30 15:19:33 +0600

    update paginate method

--- src/utils/common/pagination.ts
+++ src/utils/common/pagination.ts
@@ -1,13 +1,15 @@
-import { SelectQueryBuilder } from "typeorm";
-
-
-export function paginate<T> (
-    queryBuilder: SelectQueryBuilder<T>,
-    page: number,
-    limit: number,
-): Promise<T[]> {
-    return queryBuilder
-        .skip((page - 1) * limit)
-        .take(limit)
-        .getMany();
-}
+export class Pagination {
+    static paginate = (page = 1, limit = 10) => {
+      if (page > 0 && limit > 0 && limit < 51) {
+        const skip = page * limit - limit;
+  
+        return { page, limit: +limit, skip };
+      }
+  
+      if (+page < 1) throw new Error('Page Not Found');
+  
+      if (+limit > 50) throw new Error('Limit must be less than 50');
+  
+      if (+limit <= 0) throw new Error('Limit has limition');
+    };
+  }
